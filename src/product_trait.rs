89c5a5698f5d8b6af284e251bca018b9a7e08f1a
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-07 03:33:13 -0400

    uh

--- src/product_trait.rs
+++ src/product_trait.rs
@@ -1,6 +1,6 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, Accounts};
 
 pub trait OrbitProductTrait{
-    fn list<T>(ctx: Context<T>)-> Result<()> where for<'a> T: Accounts<'a> ;
-    fn unlist<'a, T: Accounts<'a>>(ctx: Context<T>) -> Result<()>;
+    fn list<T>(ctx: Context<T>)-> Result<()> where for<'a> T: Accounts<'a>;
+    // fn unlist<T: >(ctx: Context<T>) -> Result<()>;
 }
