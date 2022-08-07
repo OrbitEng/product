f50c5f48bd455e241be16842a99aecd393200dea
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-07 04:36:49 -0400

    uh

--- src/product_trait.rs
+++ src/product_trait.rs
@@ -1,6 +1,7 @@
 use anchor_lang::{prelude::*, Accounts};
+use crate::product_struct::OrbitProduct;
 
 pub trait OrbitProductTrait{
-    fn list<T>(ctx: Context<T>)-> Result<()> where for<'a> T: Accounts<'a>;
+    fn list<T>(ctx: Context<T>, prod: OrbitProduct)-> Result<()> where for<'a> T: Accounts<'a>;
     fn unlist<T>(ctx: Context<T>)-> Result<()> where for<'a> T: Accounts<'a>;
 }
