60d58c5e49c7bfa12dee581f6c52de3ceb09e075
Your Name <you@example.com>
2022-08-20 13:11:55 -0400

    fuck

--- src/product_struct.rs
+++ src/product_struct.rs
@@ -6,5 +6,5 @@ pub struct OrbitProduct{
     pub price: u64, // 4
     pub available: bool, // 1
     pub seller: Pubkey, // 32
-    pub delivery_estimate: i8, // rough delivery ETA // 1
+    pub delivery_estimate: u8, // rough delivery ETA // 1
 }
