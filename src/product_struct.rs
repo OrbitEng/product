dc7bd9ad2935cf467c91a85f7f858ae2f2f2810e
indianeros58 <0xDEADBEAT@gmail.com>
2022-08-25 00:05:23 -0400

    :)

--- src/product_struct.rs
+++ src/product_struct.rs
@@ -2,8 +2,8 @@ use anchor_lang::prelude::*;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
 pub struct OrbitProduct{
-    pub currency: Pubkey,
-    pub price: u64, // 4
+    pub currency: Pubkey, // 32
+    pub price: u64, // 8
     pub available: bool, // 1
     pub seller: Pubkey, // 32
     pub delivery_estimate: u8, // rough delivery ETA // 1
