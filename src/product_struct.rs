3da00dad76485df549e820599341dac7bbbcc369
indianeros58 <0xDEADBEAT@gmail.com>
2022-09-12 22:28:41 -0400

    add media field

--- src/product_struct.rs
+++ src/product_struct.rs
@@ -1,10 +1,11 @@
 use anchor_lang::prelude::*;
 
-#[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
+#[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct OrbitProduct{
     pub currency: Pubkey, // 32
     pub price: u64, // 8
     pub available: bool, // 1
     pub seller: Pubkey, // 32
     pub delivery_estimate: u8, // rough delivery ETA // 1
+    pub media: String
 }
