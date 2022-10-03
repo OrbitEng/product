0dd8f75cf2c2d5e633dfc9d2bc7ae119de68b64c
indianeros58 <0xDEADBEAT@gmail.com>
2022-10-03 16:33:45 -0400

    updates

--- src/product_struct.rs
+++ src/product_struct.rs
@@ -2,11 +2,14 @@ use anchor_lang::prelude::*;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone)]
 pub struct OrbitProduct{
-    pub info: String, // ideally max of 32 chars
+    pub info: String, // 43
+    pub owner_catalog: Pubkey, // 32
+
     pub currency: Pubkey, // 32
     pub price: u64, // 8
-    pub available: bool, // 1
-    pub seller: Pubkey, // 32
     pub delivery_estimate: u8, // rough delivery ETA // 1
     pub media: String
 }
+
+// borsh
+// 8 +  (1 + 43) | (32) | (8)  | (32) | (1) | 1 + 43
